1441583c4ed5e4e293bdd771674d44118a3c2894
Jitendra Gundaniya <jitendra_gundaniya@mckinsey.com>
2025-12-09 11:53:23 +0000

    searchable select

--- src/components/ConfigPanel/DatasetConfigForm/DatasetTypeSelect.tsx
+++ src/components/ConfigPanel/DatasetConfigForm/DatasetTypeSelect.tsx
@@ -1,33 +1,39 @@
-import React from 'react';
-import { DATASET_TYPES, groupDatasetTypesByCategory } from '../../../constants/datasetTypes';
-import type { UseFormRegister } from 'react-hook-form';
+import React, { useMemo } from 'react';
+import { DATASET_TYPES } from '../../../constants/datasetTypes';
+import { SearchableSelect } from '../../common/SearchableSelect';
+import type { SearchableSelectOption } from '../../common/SearchableSelect';
+import type { DatasetType } from '../../../types/kedro';
 
 interface DatasetTypeSelectProps {
-  register: UseFormRegister<any>;
+  value: DatasetType;
+  onChange: (value: DatasetType) => void;
 }
 
 /**
- * Reusable dataset type selector with categorized options
+ * Reusable dataset type selector with search functionality
  */
-export const DatasetTypeSelect: React.FC<DatasetTypeSelectProps> = ({ register }) => {
-  const groupedTypes = groupDatasetTypesByCategory();
+export const DatasetTypeSelect: React.FC<DatasetTypeSelectProps> = ({ value, onChange }) => {
+  // Transform dataset types into SearchableSelectOption format
+  const options: SearchableSelectOption[] = useMemo(() => {
+    return DATASET_TYPES.map((type) => ({
+      value: type.value,
+      label: type.label,
+      category: type.category,
+    }));
+  }, []);
 
   return (
     <div className="dataset-config-form__section">
       <label className="dataset-config-form__label">
         Dataset Type <span className="dataset-config-form__required">*</span>
       </label>
-      <select className="dataset-config-form__select" {...register('type', { required: true })}>
-        {Object.entries(groupedTypes).map(([category, types]) => (
-          <optgroup key={category} label={category}>
-            {types.map((type) => (
-              <option key={type.value} value={type.value}>
-                {type.label}
-              </option>
-            ))}
-          </optgroup>
-        ))}
-      </select>
+      <SearchableSelect
+        options={options}
+        value={value}
+        onChange={(newValue) => onChange(newValue as DatasetType)}
+        placeholder="Search dataset types..."
+        grouped={true}
+      />
       <span className="dataset-config-form__helper">
         Choose from {DATASET_TYPES.length} supported dataset types from kedro-datasets 3.0+.{' '}
         <a
